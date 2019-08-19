5e65b640ae2ffbde2d4289e43fc483a0fe3fead2
Josh Gustafson <josh@loomai.com>
2019-08-19 15:33:53 -0600

    fix

--- arch/strange/1.1-install-chroot.sh
+++ arch/strange/1.1-install-chroot.sh
@@ -78,20 +78,20 @@ cat <<EOF >>/etc/initcpio/hooks/mount-keys
 run_hook() {
     #ls -1 /dev/disk/by-id/*Fit*
     #ls -1 /dev/disk/by-path/*usb*
-    #ls -1 /dev/disk/by-label
+    ls -1 /dev/disk/by-label
     ls -1 /keys/*
     #mkdir -p /keys
     #mount -o ro /dev/disk/by-label/KEYS /keys
 }
 
-run_cleanuphook() {
-    #umount /keys
-}
+#run_cleanuphook() {
+#    umount /keys
+#}
 EOF
-sed -i 's/MODULES=(\(.*\))/MODULES=(\1 ext2 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /etc/mkinitcpio.conf
+sed -i 's/MODULES=(\(.*\))/MODULES=(\1 ext2 usb_storage nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /etc/mkinitcpio.conf
 sed -i 's|FILES=(\(.*\))|FILES=(\1 /keys/13)|g' /etc/mkinitcpio.conf
 #original: HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)
-sed -i 's/HOOKS=(\(.*\))/HOOKS=(base udev block autodetect modconf mount-keys zfs filesystems keyboard)/g' /etc/mkinitcpio.conf
+sed -i 's/HOOKS=(\(.*\))/HOOKS=(base udev autodetect modconf block mount-keys zfs filesystems keyboard)/g' /etc/mkinitcpio.conf
 #echo 'COMPRESSION="cat"' >>/etc/mkinitcpio.conf
 mkinitcpio -p linux-zen
 
