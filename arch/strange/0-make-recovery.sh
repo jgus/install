1094aa92097fa5a7ee92fe3aab5eca31111d73f8
Josh Gustafson <josh@loomai.com>
2019-08-21 13:35:10 -0600

    fix

--- arch/strange/0-make-recovery.sh
+++ arch/strange/0-make-recovery.sh
@@ -85,7 +85,7 @@ pacman-key --init
 pacman-key -r F75D9D76
 pacman-key --lsign-key F75D9D76
 pacman -Sy
-cd ~/install/arch/strange
+cd ~/install
 git pull
 EOF
     chmod a+x ~/archiso/airootfs/root/.runonce.sh
