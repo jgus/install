4b559dfff0e370fbcec616b5270c03957923ed37
Josh Gustafson <j@gustafson.me>
2019-08-22 09:57:54 -0600

    machine-secrets

--- arch/strange/0-read-secrets.sh
+++ arch/strange/0-read-secrets.sh
@@ -6,4 +6,4 @@ TARGET=$1
 [[ ! -d "${TARGET}" ]] || (echo "Target already exists"; exit 1)
 
 mkdir "${TARGET}"
-dd if=/sys/firmware/efi/efivars/machine-secrets-d719b2cb-3d3a-4596-a3bc-dad00e67656f bs=1 skip=4 status=none | tar -x -C "${TARGET}" --strip 1
+dd if=/sys/firmware/efi/efivars/machine-secrets-d719b2cb-3d3a-4596-a3bc-dad00e67656f bs=1 skip=4 status=none | tar -xv --zstd -C "${TARGET}" --strip 1
--- arch/strange/0-store-secrets.sh
+++ arch/strange/0-store-secrets.sh
@@ -2,11 +2,11 @@
 set -e
 
 SOURCE=$1
-[[ "${SORUCE}" != "" ]] || (echo "No source specified"; exit 1)
-[[ -d "${SORUCE}" ]] || (echo "Source does not exist"; exit 1)
+[[ "${SOURCE}" != "" ]] || (echo "No source specified"; exit 1)
+[[ -d "${SOURCE}" ]] || (echo "Source does not exist"; exit 1)
 
 TMPFILE=$(mktemp)
-tar -c --zstd -f "${TMPFILE}" "${SORUCE}"
+tar -cv --zstd -f "${TMPFILE}" "${SOURCE}"
 
 efivar -n d719b2cb-3d3a-4596-a3bc-dad00e67656f-machine-secrets -t 7 -w -f "${TMPFILE}"
 
