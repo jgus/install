d2302a4021470fc860d68e23e08e0681ba894776
Josh Gustafson <j@gustafson.me>
2019-09-02 20:32:07 -0600

    RDP

--- arch/strange/1.2-install-postboot.sh
+++ arch/strange/1.2-install-postboot.sh
@@ -49,6 +49,7 @@ PACKAGES=(
     wine wine_gecko wine-mono winetricks
     # Applications
     copyq
+    remmina freerdp
     libreoffice-still hunspell hunspell-en_US libmythes mythes-en
     scribus
     gimp
