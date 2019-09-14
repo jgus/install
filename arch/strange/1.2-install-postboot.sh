c1992ac274e5e2d8587866f740ca2dbb5fef468b
Josh Gustafson <j@gustafson.me>
2019-09-13 18:03:13 -0600

    Portainer service

--- arch/strange/1.2-install-postboot.sh
+++ arch/strange/1.2-install-postboot.sh
@@ -366,8 +366,7 @@ echo "### Configuring Docker..."
 systemctl enable --now docker.service
 systemctl enable docker-snapshot.service
 docker volume create portainer_data
-docker create --name portainer --restart always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker start portainer
+systemctl enable portainer.service
 usermod -a -G docker josh
 
 echo "### Making a snapshot..."
