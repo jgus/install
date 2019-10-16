b375fef44d42d1d7fc5429dd6b0c7849409a2c24
Josh Gustafson <j@gustafson.me>
2019-10-16 09:17:25 -0600

    update

--- arch/1.1-install-chroot.sh
+++ arch/1.1-install-chroot.sh
@@ -4,6 +4,8 @@ set -e
 HOSTNAME=$1
 source "$(cd "$(dirname "$0")" ; pwd)"/${HOSTNAME}/config.env
 
+ln -s /efi/0 /boot
+
 # Password
 cat <<EOF | passwd
 changeme
@@ -53,7 +55,6 @@ then
 fi
 
 echo "### Configuring boot image..."
-ln -s /efi/0 /target/boot
 # Initramfs
 sed -i 's/MODULES=(\(.*\))/MODULES=(\1 efivarfs)/g' /etc/mkinitcpio.conf
 [[ "${VFIO_IDS}" != "" ]] && sed -i 's/MODULES=(\(.*\))/MODULES=(\1 vfio_pci vfio vfio_iommu_type1 vfio_virqfd)/g' /etc/mkinitcpio.conf
@@ -67,8 +68,9 @@ echo "### Installing EFISTUB..."
 KERNEL_PARAMS="loglevel=3 zfs=z/root"
 [[ "${VFIO_IDS}" != "" ]] && KERNEL_PARAMS="${KERNEL_PARAMS} intel_iommu=on iommu=pt"
 KERNEL_PARAMS="${KERNEL_PARAMS} nvidia-drm.modeset=1"
-efibootmgr --disk "${SYSTEM_DEVICES[0]}" --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode "initrd=/intel-ucode.img initrd=\initramfs-linux-zen.img ${KERNEL_PARAMS}" --verbose
-efibootmgr --disk "${SYSTEM_DEVICES[0]}" --part 1 --create --label "Arch Linux (Fallback)" --loader /vmlinuz-linux-zen --unicode "initrd=/intel-ucode.img initrd=\initramfs-linux-zen-fallback.img ${KERNEL_PARAMS}" --verbose
+KERNEL_PARAMS="${KERNEL_PARAMS} initrd=/intel-ucode.img"
+efibootmgr --disk "${SYSTEM_DEVICES[0]}" --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode "${KERNEL_PARAMS} initrd=\initramfs-linux-zen.img" --verbose
+efibootmgr --disk "${SYSTEM_DEVICES[0]}" --part 1 --create --label "Arch Linux (Fallback)" --loader /vmlinuz-linux-zen --unicode "${KERNEL_PARAMS} initrd=\initramfs-linux-zen-fallback.img" --verbose
 
 echo "### Configuring nVidia updates..."
 #/etc/pacman.d/hooks/nvidia.hook
