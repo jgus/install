cd939256368a271655661017155b23b5d99a1d6f
Joshua Gustafson <josh@loomai.com>
2019-11-24 16:57:51 -0700

    tweaks

--- arch/1-install.sh
+++ arch/1-install.sh
@@ -138,8 +138,8 @@ mount -t tmpfs tmpfs /target/tmp
 df -h
 mount | grep target
 
-# echo "### Updating Pacman mirrors..."
-# curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - >/etc/pacman.d/mirrorlist
+echo "### Updating Pacman mirrors..."
+curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - >/etc/pacman.d/mirrorlist
 
 echo "### Pacstrapping..."
 pacstrap /target base ${KERNEL}
