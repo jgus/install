88a152d792070c580601121613d62b4f6fed2996
Joshua Gustafson <josh@loomai.com>
2019-12-05 14:23:35 -0700

    tweak

--- debian/0-prepare-host-debian.sh
+++ debian/0-prepare-host-debian.sh
@@ -6,6 +6,6 @@ echo "deb http://deb.debian.org/debian buster contrib" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian buster-backports main contrib" >> /etc/apt/sources.list
 uniq -i /etc/apt/sources.list
 apt update
-apt install --yes debootstrap gdisk dkms dpkg-dev linux-headers-$(uname -r)
+apt install --yes debootstrap gdisk dkms dpkg-dev linux-headers-$(uname -r) efivar
 apt install --yes -t buster-backports zfs-dkms
 modprobe zfs
