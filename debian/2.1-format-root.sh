71cfa60c16baf6fd633f4adecf0dcd1131b52a27
Josh Gustafson <j@gustafson.me>
2019-12-08 08:02:34 -0700

    tweak

--- debian/2.1-format-root.sh
+++ debian/2.1-format-root.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+KEY_FILE=/sys/firmware/efi/efivars/keyfile-77fa9abd-0359-4d32-bd60-28f4e78f784b
+
 ROOTNAME=$1
-zpool import -R /target -l z
+zpool import -N z
 zfs destroy -r z/${ROOTNAME} || true
-zfs create -o mountpoint=none z/${ROOTNAME}
-zfs set mountpoint=/ z/${ROOTNAME}
+zfs create -o encryption=on -o keyformat=raw -o keylocation=file://${KEY_FILE} -o mountpoint=/ z/${ROOTNAME}
 zpool export z
