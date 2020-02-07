dda45dc3be5d23c0e001d329f17e488fec29f314
Josh Gustafson <j@gustafson.me>
2020-02-07 16:11:03 -0700

    Ubuntu WIP

---
+++ ubuntu/0-prepare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# sudo -i
+# git clone https://github.com/jgus/install
+
+echo "### Adding packages..."
+PACKAGES=(
+    debootstrap
+    gdisk
+    zfsutils-linux
+    openssh-server
+)
+apt-add-repository universe
+apt update
+apt install --yes "${PACKAGES[@]}"
+
+mkdir -p ~/.ssh || true
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+curl https://github.com/jgus.keys >> ~/.ssh/authorized_keys
+uniq -i ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
+
+systemctl start ssh
