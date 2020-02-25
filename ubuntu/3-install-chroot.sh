539cabd15763bee30da22f5a09d14e49846270c7
Josh Gustafson <j@gustafson.me>
2020-02-25 07:56:02 -0700

    fix

--- ubuntu/3-install-chroot.sh
+++ ubuntu/3-install-chroot.sh
@@ -145,7 +145,7 @@ echo "### Configuring Printer Driver..."
 cd /tmp
 curl -L -O http://gdlp01.c-wss.com/gds/6/0100009236/06/linux-UFRII-drv-v510-usen-09.tar.gz
 tar xvf linux-UFRII-drv-v510-usen-09.tar.gz
-echo "y\nn" | ./linux-UFRII-drv-v510-usen/install.sh
+{ echo y ; echo n ; } | ./linux-UFRII-drv-v510-usen/install.sh
 
 echo "### Configuring Docker..."
 #/etc/docker/daemon.json
