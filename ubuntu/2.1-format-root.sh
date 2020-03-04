29e4613eb7c8a98e07fa5deb79b77bf3f4b8ea5b
Josh Gustafson <j@gustafson.me>
2020-03-04 13:18:47 -0700

    fix

--- ubuntu/2.1-format-root.sh
+++ ubuntu/2.1-format-root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-zpool import -R /target -N root
+zpool import -R /target -l -N root
 zfs destroy -r root/root || true
 zfs create -o mountpoint=none root/root
 zfs set mountpoint=/ root/root || true
