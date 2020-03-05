88f7dee167f64665e1be3e139785b261148cd090
Josh Gustafson <j@gustafson.me>
2020-03-05 11:05:57 -0700

    fix

--- ubuntu/3-install-chroot.sh
+++ ubuntu/3-install-chroot.sh
@@ -143,7 +143,7 @@ fi
 
 echo "### Installing bootloader..."
 echo "efivars" >>/etc/initramfs-tools/modules
-update-initramfs
+update-initramfs -u
 mv /etc/default/grub /etc/default/grub.dist
 mv /etc/default/grub.new /etc/default/grub
 update-grub
