4121a65c69d37e3d17c34acafe60a47ecb27a5b4
Josh Gustafson <j@gustafson.me>
2020-05-04 09:25:27 -0600

    jarvis

---
+++ ubuntu/jarvis/files/usr/local/bin/sync-to-nas.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$( dirname "${BASH_SOURCE[0]}" )/functions.sh"
+
+for bak in /boot/bak*
+do
+    [ -d "${bak}" ] || continue
+    rsync -arPx --delete /boot/ "${bak}"
+done
+rsync -arPx --delete /boot/ /e/$(hostname)/boot
+
+#DATASETS=($(zfs list -o name | grep -v root/docker))
+DATASETS=(root)
+
+for x in "${DATASETS[@]}"
+do
+    zfs_send_new_snapshots "" ${x} "" e/$(hostname)/${x}
+done
+
+DATASETS=(root)
+for s in backup git home photos projects images volumes
+do
+    DATASETS+=($(zfs list -H -o name -r d/${s}))
+done
+
+for x in "${DATASETS[@]}"
+do
+    zfs_send_new_snapshots "" ${x} "" b/${x}
+done
