b77c5dc74cae5662c88175f735cc13e95581c974
Josh Gustafson <j@gustafson.me>
2020-09-21 17:10:07 -0600

    /usr/local/bin

---
+++ arch/common/files/usr/local/bin/sync-to-nas.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$( dirname "${BASH_SOURCE[0]}" )/functions.sh"
+
+mirror_boot
+
+DATASETS=($(zfs list -o name | grep -v rpool/docker))
+
+for x in "${DATASETS[@]}"
+do
+    zfs_send_new_snapshots "" ${x} root@nas e/$(hostname)/${x}
+done
