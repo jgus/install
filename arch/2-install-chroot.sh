b8944d163989a2bbbf20983f3e5726f9177bb096
Josh Gustafson <j@gustafson.me>
2020-09-25 14:26:24 -0600

    fix

--- arch/2-install-chroot.sh
+++ arch/2-install-chroot.sh
@@ -110,19 +110,12 @@ Operation=Upgrade
 Operation=Remove
 Type=Package
 Target=nvidia-dkms
-EOF
-for k in "${KERNELS[@]}"
-do
-    echo "Target=${k}" >>/etc/pacman.d/hooks/nvidia.hook
-done
-cat << EOF >>/etc/pacman.d/hooks/nvidia.hook
 
 [Action]
 Description=Update Nvidia module in initcpio
 Depends=mkinitcpio
 When=PostTransaction
-NeedsTargets
-Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+Exec=/usr/bin/mkinitcpio -P
 EOF
 
 echo "### Configuring VFIO..."
