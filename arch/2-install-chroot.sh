05ddf88105beddb1015e04909efde35a52644c50
Josh Gustafson <j@gustafson.me>
2020-09-25 10:31:06 -0600

    fix

--- arch/2-install-chroot.sh
+++ arch/2-install-chroot.sh
@@ -109,7 +109,7 @@ Operation=Install
 Operation=Upgrade
 Operation=Remove
 Type=Package
-Target=${NVIDIA_PACAKGE}
+Target=nvidia-dkms
 EOF
 for k in "${KERNELS[@]}"
 do
@@ -122,7 +122,7 @@ Description=Update Nvidia module in initcpio
 Depends=mkinitcpio
 When=PostTransaction
 NeedsTargets
-Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
 EOF
 
 echo "### Configuring VFIO..."
