486e4507ce69e368e58e1ed85bd7c25b68e49943
Josh Gustafson <j@gustafson.me>
2020-10-18 16:48:20 -0600

    verbose

--- arch/spidey/overrides.sh
+++ arch/spidey/overrides.sh
@@ -8,10 +8,14 @@ do_partition() {
         wipefs -af "${DEVICE}"
         parted -s "${DEVICE}" -- mklabel gpt
         while [ -L "${DEVICE}-part2" ] ; do : ; done
+        echo "### Creating boot partition on ${DEVICE}..."
         parted -s -a optimal "${DEVICE}" -- mkpart primary fat32 '0%' "${BOOT_SIZE}GiB"
         parted -s "${DEVICE}" -- set 1 esp on
+        echo "### Creating ZFS partition on ${DEVICE}..."
         parted -s -a optimal "${DEVICE}" -- mkpart primary "${BOOT_SIZE}GiB" "-$((SWAP_SIZE+WINDOWS_SIZE))GiB"
+        echo "### Creating swap partition on ${DEVICE}..."
         parted -s -a optimal "${DEVICE}" -- mkpart primary ntfs "-$((SWAP_SIZE+WINDOWS_SIZE))GiB" "-${WINDOWS_SIZE}GiB"
+        echo "### Creating Windows partition on ${DEVICE}..."
         parted -s -a optimal "${DEVICE}" -- mkpart primary ntfs "-${WINDOWS_SIZE}GiB" '100%'
         sleep 1
         BOOT_DEVS+=("${DEVICE}-part1")
