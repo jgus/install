2445f0435c589ba496a5dde2cd699cb0f33832f0
Josh Gustafson <j@gustafson.me>
2020-10-19 17:18:23 -0600

    tweak?

--- arch/spidey/install-postboot/5-system76.sh
+++ arch/spidey/install-postboot/5-system76.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-install system76-driver system76-dkms system76-io-dkms system76-firmware-daemon system76-power
+install system76-driver system76-dkms system76-io-dkms system76-firmware-daemon #system76-power
