1e015c38c83aa89cb9054824851577b8c866015d
Josh Gustafson <j@gustafson.me>
2020-10-24 09:53:52 -0600

    update

--- arch/install-postboot/5-optimus.sh
+++ arch/install-postboot/5-optimus.sh
@@ -2,8 +2,10 @@
 
 if ((HAS_OPTIMUS))
 then
-    install optimus-manager optimus-manager-qt
-    systemctl enable optimus-manager.service
+    install nvidia-prime
+
+    # install optimus-manager optimus-manager-qt
+    # systemctl enable optimus-manager.service
 
 #     cat << EOF >> /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf || true
 # Section "OutputClass"
@@ -29,37 +31,37 @@ then
 # xrandr --dpi 96
 # EOF
 
-    mkdir -p /lib/udev/rules.d
-    cat << EOF > /lib/udev/rules.d/80-nvidia-pm.rules
-# Remove NVIDIA USB xHCI Host Controller devices, if present
-#ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
+#     mkdir -p /lib/udev/rules.d
+#     cat << EOF > /lib/udev/rules.d/80-nvidia-pm.rules
+# # Remove NVIDIA USB xHCI Host Controller devices, if present
+# #ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
 
-# Remove NVIDIA USB Type-C UCSI devices, if present
-#ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
+# # Remove NVIDIA USB Type-C UCSI devices, if present
+# #ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
 
-# Remove NVIDIA Audio devices, if present
-#ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
+# # Remove NVIDIA Audio devices, if present
+# #ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
 
-# Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
-ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+# # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+# ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+# ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
 
-# Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-EOF
+# # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
+# ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+# ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+# EOF
 
-    cat << EOF > /etc/modprobe.d/nvidia.conf
-options nvidia "NVreg_DynamicPowerManagement=0x02"
-EOF
+#     cat << EOF > /etc/modprobe.d/nvidia.conf
+# options nvidia "NVreg_DynamicPowerManagement=0x02"
+# EOF
 
-    mkdir -p /etc/optimus-manager
-    cat << EOF > /etc/optimus-manager/optimus-manager.conf
-[optimus]
-startup_mode=auto
-startup_auto_battery_mode=hybrid
-startup_auto_extpower_mode=nvidia
-EOF
+#     mkdir -p /etc/optimus-manager
+#     cat << EOF > /etc/optimus-manager/optimus-manager.conf
+# [optimus]
+# startup_mode=auto
+# startup_auto_battery_mode=hybrid
+# startup_auto_extpower_mode=nvidia
+# EOF
 
-    /usr/local/bin/update-efiboot.sh
+#     /usr/local/bin/update-efiboot.sh
 fi
