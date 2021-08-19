d86eff26b5759f1bad5b74eeda657dd041059b11
Josh Gustafson <jgustafson@snap.com>
2021-08-18 18:23:23 -0600

    disable

--- arch/install-postboot/5-printing.sh
+++ arch/install-postboot/5-printing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ((HAS_GUI))
 then
-    install cups cups-pdf ghostscript gsfonts cnrdrvcups-lb-bin
+    install cups cups-pdf ghostscript gsfonts #cnrdrvcups-lb-bin
     systemctl enable cups
 fi
