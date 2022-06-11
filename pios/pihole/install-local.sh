a6b962f3a8f47d5a7f9c3ead9e7340ff80aedcc9
Josh Gustafson <j@gustafson.me>
2022-06-11 07:32:47 -0600

    tweak

--- pios/pihole/install-local.sh
+++ pios/pihole/install-local.sh
@@ -16,3 +16,12 @@ apt upgrade --yes --allow-downgrades
 apt install --yes "${PACKAGES[@]}"
 
 bash <(curl -sSL https://install.pi-hole.net)
+
+cat << EOF >/etc/dhcpcd.conf
+slaac private
+
+interface eth0
+static ip_address=172.22.0.2/16
+static routers=172.22.0.1
+static domain_name_servers=127.0.0.1
+EOF
