6a54a46ff28354bd50c5f6efeff7399f996a1934
Josh Gustafson <j@gustafson.me>
2022-06-18 15:44:07 -0600

    zfs

--- nixos/0-format-root-zfs.sh
+++ nixos/0-format-root-zfs.sh
@@ -17,14 +17,16 @@ ZPOOL_OPTS=(
 
 zpool create -f "${ZPOOL_OPTS[@]}" rpool /dev/disk/by-partlabel/root
 
-zfs create -o com.sun:auto-snapshot=false   -o canmount=off                         rpool/var
-zfs create                                                                          rpool/var/cache
-zfs create                                                                          rpool/var/log
-zfs create                                                                          rpool/var/spool
-zfs create                                                                          rpool/var/tmp
-#zfs create -o com.sun:auto-snapshot=false   -o mountpoint=/var/lib/docker           rpool/docker
-#zfs create                                  -o mountpoint=/var/volumes              rpool/volumes
-#zfs create -o com.sun:auto-snapshot=false                                           rpool/volumes/scratch
-#zfs create                                  -o mountpoint=/var/lib/libvirt/images   rpool/images
-#zfs create -o com.sun:auto-snapshot=false                                           rpool/images/scratch
+# zfs create -o com.sun:auto-snapshot=false   -o canmount=off                         rpool/var
+# zfs create                                                                          rpool/var/cache
+# zfs create                                                                          rpool/var/log
+# zfs create                                                                          rpool/var/spool
+# zfs create                                                                          rpool/var/tmp
+# zfs create -o com.sun:auto-snapshot=false   -o mountpoint=/var/lib/docker           rpool/docker
+# zfs create                                  -o mountpoint=/var/volumes              rpool/volumes
+# zfs create -o com.sun:auto-snapshot=false                                           rpool/volumes/scratch
+# zfs create                                  -o mountpoint=/var/lib/libvirt/images   rpool/images
+# zfs create -o com.sun:auto-snapshot=false                                           rpool/images/scratch
+zfs create                                   -o mountpoint=/etc/nixos               rpool/nixos
 zfs create                                                                          rpool/home
+zfs create                                   -o mountpoint=/root                    rpool/home/root
