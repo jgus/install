11a8f8a7e56f80d2453ed2e5de6580b3a2ba9bb3
Josh Gustafson <j@gustafson.me>
2022-06-23 07:37:32 -0600

    zfs

--- nixos/common/etc/nixos/zfs.nix
+++ nixos/common/etc/nixos/zfs.nix
@@ -7,13 +7,37 @@
     ];
     zfs = {
       devNodes = "/dev/disk/by-path";
-      #extraPools = [ "d" ];
+      extraPools = [ "rpool" ];
     };
   };
 
   environment.systemPackages = with pkgs; [
     zfs
+    zfs-autobackup
   ];
 
   services.zfs.autoScrub.enable = true;
+
+  systemd = {
+    services = {
+      zfs-auto-snapshot = {
+        path = with pkgs; [ hostname zfs zfs-autobackup ];
+        script = "zfs-autobackup --keep-source 10,1h2d,1d2w,1w2m,1m1y snap-$(hostname)";
+        serviceConfig = {
+          Type = "oneshot";
+        };
+      };
+    };
+    timers = {
+     zfs-auto-snapshot = {
+        wantedBy = [ "timers.target" ];
+        partOf = [ "zfs-auto-snapshot.service" ];
+        timerConfig = {
+          OnCalendar = "hourly";
+          Persistent = true;
+          Unit = "zfs-auto-snapshot.service";
+        };
+      };
+    };
+  };
 }
